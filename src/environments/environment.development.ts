29804f462e2768c08ec8657d182f8c08732ab736
Mauricio Noscue <mauronoscue@gmail.com>
2025-10-30 02:18:58 -0500

    inital jeankinfile-v23

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
-  apiURL: (window as any)['env']?.API_BASE_URL || 'http://appointments-api-staging:8080',
+  apiURL: (window as any)['env']?.API_BASE_URL || 'http://appointments-api-staging:5100',
   hubs: {
     appointments: '/hubs/appointments'
   }
