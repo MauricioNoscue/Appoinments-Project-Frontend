7c09d6f7d5472c2cec3a08708a457898deb10534
Mauricio Noscue <mauronoscue@gmail.com>
2025-10-30 02:37:36 -0500

    cam4

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
-  apiURL: (window as any)['env']?.API_BASE_URL || 'http://appointments-api-staging:5100',
+  apiURL: (window as any)['env']?.API_BASE_URL || 'API_BASE_URL=http://localhost:5100',
   hubs: {
     appointments: '/hubs/appointments'
   }
