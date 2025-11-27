d734341ea43e5f98199f73928007e046b50a075f
Mauricio Noscue <mauronoscue@gmail.com>
2025-11-27 18:40:15 -0500

    cambios enviromet

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,6 +1,7 @@
 export const environment = {
  production: true,
-  apiURL: (window as any)['env']?.API_BASE_URL || 'API_BASE_URL=http://localhost:5200',
+   apiURL: 'https://98.95.54.163',
+
   hubs: {
     notification: '/hubs/noti', 
 
