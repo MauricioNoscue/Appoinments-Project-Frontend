8c6f382f57ce742baa3fd602728825f165e380bf
Mauricio Noscue <mauronoscue@gmail.com>
2025-12-01 22:05:30 -0500

    cambios en environment

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,6 +1,7 @@
 export const environment = {
  production: true,
-  apiURL: (window as any)['env']?.API_BASE_URL || 'API_BASE_URL=http://localhost:5200',
+    apiURL: 'https://api.cita-hospital.site',
+
   hubs: {
     notification: '/hubs/noti', 
 
